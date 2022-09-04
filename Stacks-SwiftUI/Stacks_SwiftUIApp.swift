4adfff73a5044d77a9b58627f39414b655f6b9ed
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2022-09-04 09:26:29 +0200

    Initial Commit

---
+++ Stacks-SwiftUI/Stacks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stacks_SwiftUIApp.swift
+//  Stacks-SwiftUI
+//
+//  Created by Macbook on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Stacks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
