65c3f73fc9aed0f21d508afbd5b3d4c2eda10d2f
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-07 14:10:08 +0200

    Organizing Code.

--- Stacks-SwiftUI/ContentView.swift
+++ Stacks-SwiftUI/ContentView.swift
@@ -12,36 +12,8 @@ struct ContentView: View {
             VStack{
                 HeaderView() // Code in Content View now is being much cleaner and easier to read.
                 HStack { //Embed VStack in HStack
-                    VStack {
-                        Text("Basic")
-                            .font(.system(.title, design: .rounded))
-                            .fontWeight(.black)
-                        Text("$9")
-                            .font(.system(size: 40, weight: .heavy, design: .rounded))
-                        Text("per month")
-                            .font(.headline)
-                    }
-                    .foregroundColor(.white)
-                    //Using Frame in order to make Vstack width indpendent on Text Length..
-                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)// adjust max width to infinty instead of fixed number means the view will adjust itself to fill maximum width..
-                    .padding(40) // Padding Must Get first..
-                    .background(Color.purple)
-                    .cornerRadius(10)
-                    //The both Vstack looks very similar but Actually they are dpending on length of text.
-                    VStack {
-                        Text("Pro")
-                            .font(.system(.title, design: .rounded))
-                            .fontWeight(.black)
-                        Text("$19")
-                            .font(.system(size: 40, weight: .heavy, design: .rounded))
-                        Text("per month")
-                            .font(.headline)
-                            .foregroundColor(.gray)
-                    }
-                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
-                    .padding(40)
-                    .background(Color(red: 240/255, green: 240/255, blue: 240/255))
-                    .cornerRadius(10)
+                    PricingView(title: "Basic", Price: "$9", TextColor: .white, bgColor: .purple)
+                    PricingView(title: "Pro", Price: "$19", TextColor: .black, bgColor: Color(red: 240/255, green: 240/255, blue: 240/255))
             }
                 .padding(.horizontal, 20)
         }
@@ -66,5 +38,30 @@ struct HeaderView: View {
                 .font(.system(.largeTitle, design: .rounded))
                 .fontWeight(.black)
         }
+    }
+}
+//--------------------------------------------
+//Extract Subview with arguments.
+struct PricingView: View {
+    var title : String
+    var Price : String
+    var TextColor : Color
+    var bgColor : Color
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+            Text(Price)
+                .font(.system(size: 40, weight: .heavy, design: .rounded))
+            Text("per month")
+                .font(.headline)
+        }
+        .foregroundColor(TextColor)
+        //Using Frame in order to make Vstack width indpendent on Text Length..
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)// adjust max width to infinty instead of fixed number means the view will adjust itself to fill maximum width..
+        .padding(40) // Padding Must Get first..
+        .background(bgColor)
+        .cornerRadius(10)
     }
 }
