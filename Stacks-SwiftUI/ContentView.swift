6be0aceb28e35d41638d53ba117dbb6bd979dd0f
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-07 14:20:00 +0200

    Z-Stack

--- Stacks-SwiftUI/ContentView.swift
+++ Stacks-SwiftUI/ContentView.swift
@@ -11,10 +11,21 @@ struct ContentView: View {
     var body: some View {
             VStack{
                 HeaderView() // Code in Content View now is being much cleaner and easier to read.
-                HStack { //Embed VStack in HStack
+                HStack(spacing :15) { //Embed VStack in HStack
                     PricingView(title: "Basic", Price: "$9", TextColor: .white, bgColor: .purple)
+                    //Using Z-Stack.
+                    ZStack {
                     PricingView(title: "Pro", Price: "$19", TextColor: .black, bgColor: Color(red: 240/255, green: 240/255, blue: 240/255))
-            }
+                        Text("Best for Designers")
+                            .font(.system(.caption, design: .rounded))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding(5)// Padding comes before background color
+                            .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+                            .offset(x: 0, y: 87) // To Adjust Position of Text.
+                        //Positive Value to move down and negative value to move up
+                    }
+                }
                 .padding(.horizontal, 20)
         }
     }
